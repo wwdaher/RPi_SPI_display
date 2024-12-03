c4e9e8a58cd56fad12807b5ab7515128175cf3d7
wwdaher <purchases@dahertribe.com>
2024-12-03 10:40:51 +0100

    add support for ini file

--- monitorscreeno.py
+++ monitorscreeno.py
@@ -1,6 +1,18 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
+#---------------------------------------------------------------------
+# monitorscreeno: object oriented version of a performance monitor
+# 		  screen for a raspberry pi running a 2.4" TFT
+# 		  at 320x240
+# 		  Config file monitorscreeno.ini must be present
+#
+#		  GPL license applies
+#
+# author: 
+# 	wdaher
+# version:
+# 	0.0	2024/11/01
+#	1.0	2024/12/03	add monitorscreeno.ini support
+# 	
+#---------------------------------------------------------------------
 import socket
 from netifaces import interfaces, ifaddresses, AF_INET
 import digitalio
@@ -16,35 +28,41 @@ from xpt2046 import Touch
 from gpiozero import Button, DigitalOutputDevice
 from mygaugeso import Bar, TextBox, Dial, Gauge, BarMulti
 from random import randrange
+import configparser
+import sys
 
-# First define some constants to allow easy resizing of shapes.
-BORDER = 20
-FONTSIZE = 12
+#---------------------------------------------------------------------
+# read the config file
+#---------------------------------------------------------------------
+config = configparser.ConfigParser()
+config.read('monitorscreeno.ini')
+SCREENBACKGROUNDCOLOR = ImageColor.getrgb(config['default']['screenbackgroundcolor'])
+try:
+	SCREENBACKGROUNDIMAGE = config['default']['screenbackgroundimage']
+except:
+	SCREENBACKGROUNDIMAGE = ''
+OBJBORDERCOLOR = ImageColor.getrgb(config['default']['objbordercolor'])
+OBJBACKGROUNDCOLOR = ImageColor.getrgb(config['default']['objbackgroundcolor'])
+GAUGEDIALCOLOR = ImageColor.getrgb(config['default']['gaugedialcolor'])
 
-SCREENBACKGROUNDCOLOR = ImageColor.getrgb("salmon")
-OBJBORDERCOLOR = ImageColor.getrgb("lightblue")
-OBJBACKGROUNDCOLOR = ImageColor.getrgb("lightsalmon")
-GAUGEDIALCOLOR = ImageColor.getrgb("black")
+UPDATEFREQUENCY = int(config['default']['updatefrequency'])
+DEFAULTFONTLOCATION = config['default']['fontlocation']
+FONTSIZE = int(config['default']['fontsize'])
+
+NET_MAXBYTES_PER_SECOND = int(config['default']['net_maxbytes_per_second'])     # 30Mb/sec
+DISK_MAXBYTES_PER_SECOND = int(config['default']['disk_maxbytes_per_second'])     # 30Mb/sec
 
 
-#DEFAULTFONTLOCATION = "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
-#DEFAULTFONTLOCATION = "/usr/share/fonts/truetype/droid/DroidSansFallbackFull.ttf"
-#DEFAULTFONTLOCATION = "/usr/share/fonts/truetype/freefont/FreeSans.ttf"
-#DEFAULTFONTLOCATION = "/usr/share/fonts/truetype/freefont/FreeSerif.ttf"
-#DEFAULTFONTLOCATION = "/usr/share/fonts/truetype/liberation/LiberationMono-Regular.ttf"
-#DEFAULTFONTLOCATION = "/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf"
-#DEFAULTFONTLOCATION = "/usr/share/fonts/truetype/liberation/LiberationSansNarrow-Regular.ttf"
-#DEFAULTFONTLOCATION = "/usr/share/fonts/truetype/liberation/LiberationSerif-Regular.ttf"
-#DEFAULTFONTLOCATION = "/usr/share/fonts/truetype/liberation2/LiberationMono-Regular.ttf"
-#DEFAULTFONTLOCATION = "/usr/share/fonts/truetype/liberation2/LiberationSans-Regular.ttf"
-#DEFAULTFONTLOCATION = "/usr/share/fonts/truetype/liberation2/LiberationSansNarrow-Regular.ttf"
-#DEFAULTFONTLOCATION = "/usr/share/fonts/truetype/liberation2/LiberationSerif-Regular.ttf"
-#DEFAULTFONTLOCATION = "/usr/share/fonts/truetype/noto/NotoMono-Regular.ttf"
-#DEFAULTFONTLOCATION = "/usr/share/fonts/truetype/piboto/Piboto-Regular.ttf"
-#DEFAULTFONTLOCATION = "/usr/share/fonts/truetype/piboto/Piboto-Light.ttf"
-DEFAULTFONTLOCATION = "/usr/share/fonts/truetype/quicksand/Quicksand-Regular.ttf"
+BAUDRATE = int(config['default']['screen_baudrate'])
+
+#---------------------------------------------------------------------
+# set up default font
+#---------------------------------------------------------------------
 DEFAULTFONT = ImageFont.truetype(DEFAULTFONTLOCATION, FONTSIZE)
 
+#---------------------------------------------------------------------
+# get information about network adapters and addresses
+#---------------------------------------------------------------------
 def getIPData():
     IPAddr = socket.gethostbyname(hostname)
 
@@ -60,8 +78,9 @@ def getIPAddrs():
             IPAddrs += ifaceName + ": " + " ".join(addresses) + ", "
     return IPAddrs[:-2]
 
-NET_MAXBYTES_PER_SECOND = 30000000     # 30Mb/sec
-DISK_MAXBYTES_PER_SECOND = 30000000    # 30Mb/sec
+#---------------------------------------------------------------------
+# calculate disk and network activityi
+#---------------------------------------------------------------------
 prev_net_bytes = 0
 prev_net_time = time_ns()
 prev_disk_bytes = 0
@@ -97,13 +116,13 @@ def calcIO():
 
 
 
+#---------------------------------------------------------------------
+# configure details about the screen and how its connected
+#---------------------------------------------------------------------
 # Configuration for CS and DC pins (these are PiTFT defaults):
 cs_pin = digitalio.DigitalInOut(board.CE0)
 dc_pin = digitalio.DigitalInOut(board.D25)
 reset_pin = digitalio.DigitalInOut(board.D24)
-
-# Config for display baudrate (default max is 24mhz):
-BAUDRATE = 24000000
 
 # Setup SPI bus using hardware SPI:
 spi = board.SPI()
@@ -130,8 +149,9 @@ else:
     height = disp.height
 
 
-#--------------------------------------------------------------
+#---------------------------------------------------------------------
 # now all the touch stuff
+#---------------------------------------------------------------------
 # add to /boot/firmware/config.txt: dtoverlay=spi1-1cs
 #   connect cs cable to GPIO18, not 17 as code says
 #these two need to be at the top
@@ -160,23 +180,30 @@ irq = Button(26)
 spi = busio.SPI(clk, mosi, miso)	# auxiliary SPI
 
 xpt = Touch(spi, cs=cs, int_pin=irq, int_handler=touchscreen_press)
+#---------------------------------------------------------------------
 # end of touch stuff
-#--------------------------------------------------------------
+#---------------------------------------------------------------------
+
+#---------------------------------------------------------------------
 
 #fill entire screen with background
 #image = Image.open('roundlines.png')
-image = Image.open('fd3.png')
-image.putalpha(1)
-disp.image(image)
+if SCREENBACKGROUNDIMAGE != '':
+	image = Image.open(SCREENBACKGROUNDIMAGE)
+	disp.image(image)
+else:
 
-#image = Image.new("RGBA", (width, height))
-#image.putalpha(28)
-#draw.rectangle(xy = (0, 0, width, height),
-#               fill=SCREENBACKGROUNDCOLOR
-#               )
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
+	image = Image.new("RGBA", (width, height))
+	# Get drawing object to draw on image.
+	draw = ImageDraw.Draw(image)
 
+	draw.rectangle(xy = (0, 0, width, height),
+               fill=SCREENBACKGROUNDCOLOR
+               )
+
+#---------------------------------------------------------------------
+# divide the screen into two small, two medium and one large row
+#---------------------------------------------------------------------
 rowcount = 5
 rowtop = [0]*rowcount
 rowbottom = [0]*rowcount
@@ -197,7 +224,9 @@ for i in range(1, 5):
     rowtop[i] = rowtop[i-1] + rowheight[i-1] + rowspace
     rowbottom[i] = rowtop[i] + rowheight[i]
 
-#display the host name
+#---------------------------------------------------------------------
+# display the host name
+#---------------------------------------------------------------------
 hostname = socket.gethostname()
 hostnametb = TextBox(image, (rowleftmargin,rowtop[0]), (rowrightright, rowbottom[0]))
 hostnametb.set_text("Hostname: " + hostname)
@@ -207,6 +236,9 @@ hostnametb.set_text_color(GAUGEDIALCOLOR)
 hostnametb.set_font_loc(DEFAULTFONTLOCATION)
 hostnametb.refresh()
 
+#---------------------------------------------------------------------
+# textbox with the IP addresses
+#---------------------------------------------------------------------
 IPs = getIPAddrs()
 iptb = TextBox(image, (rowleftmargin,rowtop[1]), (rowrightright, rowbottom[1]))
 iptb.set_text(IPs)
@@ -217,6 +249,9 @@ iptb.set_font_loc(DEFAULTFONTLOCATION)
 iptb.set_font_size(16)
 iptb.refresh()
 
+#---------------------------------------------------------------------
+# Bargraph with network and disk activity
+#---------------------------------------------------------------------
 iobc = BarMulti(image,(rowleftmargin, rowtop[2]), (rowrightright, rowbottom[2]), 2)
 iobc.set_title("I/O")
 iobc.set_title_color(ImageColor.getrgb('black'))
@@ -231,6 +266,9 @@ iobc.set_gradient_colors(ImageColor.getrgb("pink"), ImageColor.getrgb("purple"))
 iobc.set_gradient(True)
 iobc.set_barempty_color(ImageColor.getrgb("lightslategray"))
 
+#---------------------------------------------------------------------
+# gauge wtih the amount of virtual memory used
+#---------------------------------------------------------------------
 perg = Gauge(image,(rowleftmargin,rowtop[3]), (rowleftright,rowbottom[3]))
 perg.set_title("Virtual Memory")
 perg.set_background_color(OBJBACKGROUNDCOLOR)
@@ -243,6 +281,9 @@ perg.set_dialempty_color(ImageColor.getrgb("lightslategray"))
 
 
 
+#---------------------------------------------------------------------
+# gauge wtih CPU temperatur
+#---------------------------------------------------------------------
 tempg = Gauge(image,(rowrightmargin,rowtop[3]), (rowrightright,rowbottom[3]))
 tempg.set_title("CPU Temp")
 tempg.set_background_color(OBJBACKGROUNDCOLOR)
@@ -254,6 +295,9 @@ tempg.set_outline(GAUGEDIALCOLOR)
 tempg.set_dialempty_color(ImageColor.getrgb("lightslategray"))
 
 
+#---------------------------------------------------------------------
+# dial for total CPU activity
+#---------------------------------------------------------------------
 cpud = Dial(image,(rowleftmargin, rowtop[4]), 105)
 cpud.set_title("CPU")
 cpud.set_background_color(OBJBACKGROUNDCOLOR)
@@ -266,6 +310,9 @@ cpud.set_gradient_colors(ImageColor.getrgb("yellow"), ImageColor.getrgb("red"))
 cpud.set_gradient(True)
 cpud.set_dialempty_color(ImageColor.getrgb("lightslategray"))
 
+#---------------------------------------------------------------------
+# bargraph with activity for individual CPU cores
+#---------------------------------------------------------------------
 cpur = BarMulti(image,(rowrightmargin, rowtop[4]), (rowrightright, rowbottom[4]), 4)
 cpur.set_title("CPU Cores")
 cpur.set_title_color(ImageColor.getrgb('black'))
@@ -281,12 +328,13 @@ cpur.set_gradient_colors(ImageColor.getrgb("lightgreen"), ImageColor.getrgb("bla
 cpur.set_gradient(True)
 cpur.set_barempty_color(ImageColor.getrgb("lightslategray"))
 
-updatefrequency = 1 # in seconds
+#---------------------------------------------------------------------
+# now loop forever, each time displaying updated values
+#---------------------------------------------------------------------
 
 while 1:
 
     virtualpercent = psutil.virtual_memory().percent 
-#    virtualpercent = randrange(100) 
     perg.set_percent(virtualpercent)
     perg.set_text(str(virtualpercent) + '%')
 
@@ -295,44 +343,27 @@ while 1:
     tempg.set_text(str(math.trunc(cpu_temp.current))+"°C")
 
     calcIO()
-#    networkio = randrange(100)
-#    diskio = randrange(100)
     iobc.set_percent((net_percent, disk_percent))
 
-    cpupercent = psutil.cpu_percent(interval=updatefrequency) 
-#    cpupercent = randrange(100) 
+    cpupercent = psutil.cpu_percent(interval=UPDATEFREQUENCY) 
     cpud.set_percent(cpupercent)
     cpud.set_text(str(cpupercent)+"%")
 
-    rawcpumpercent = psutil.cpu_percent(interval=updatefrequency, percpu=True)
+    rawcpumpercent = psutil.cpu_percent(interval=UPDATEFREQUENCY, percpu=True)
     cpumpercent = ()
     for i in range(0, len(rawcpumpercent)):  
         cpumpercent = cpumpercent + (int(rawcpumpercent[i]),)
     cpur.set_percent(cpumpercent)
-#    cpur.set_percent((randrange(100), randrange(100), randrange(100), randrange(100)))
     image.putalpha(28)
     disp.image(image)
 
-    sleep(updatefrequency)
-
-"""
-    mygauges.mydial(image=image, 
-                     topleft=(60,190), 
-                     dialsize=120,
-                     percent=cpupercent ,
-                     text=str(cpupercent)+"%" ,
-                     title="CPU",
-                     backgroundcolor=OBJBACKGROUNDCOLOR,
-                     bordercolor=OBJBORDERCOLOR,
-                     textcolor=ImageColor.getrgb("green"),
-                     fontloc=DEFAULTFONTLOCATION,
-                     outline=ImageColor.getrgb("darkgreen"),
-                    )
-"""
+    sleep(UPDATEFREQUENCY)
 
 
 
-
+#---------------------------------------------------------------------
+# following code only for testing purposes
+#---------------------------------------------------------------------
 
 
 for cpupercent in range(0, 101, 10):
