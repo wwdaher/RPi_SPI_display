3b4d7038486441975bae50c3b1d621ad074c724f
wwdaher <53574837+wwdaher@users.noreply.github.com>
2024-12-18 16:26:52 +0100

    add codelocation changes

--- startmon.sh
+++ startmon.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 startcommand="python3 monitorscreen.py"
+codelocation="/home/wdaher/pgms/python/spidisplay"
 
 if pgrep -f "$startcommand" > /dev/null
 then
     echo "System monitor already running"
 else
     echo "Starting system monitor"
-    cd ~/pgms/python/spidisplay
+    cd "$codelocation"
     $startcommand 2>/dev/null &
 fi
